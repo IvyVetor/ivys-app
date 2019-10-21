b30aa2f19b409c705b6601de5d954987c512fec9
Ivy Vetor <ivyvetor@gmail.com>
2019-10-20 21:03:47 -0400

    lectures 21 - 25

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,19 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', // element
   // selector: '[app-servers]', // attribute
   // selector: '.app-servers', // class
-  template: `
-      <app-server></app-server>
-      <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  //     <app-server></app-server>
+  //     <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
